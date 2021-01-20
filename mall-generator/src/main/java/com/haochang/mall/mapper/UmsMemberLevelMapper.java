f7c2fde1c3c8ef73c1c9d6875de8bacb5b6a36e0
youzhi.gao <youzhi.gao@ikang.com>
2021-01-20 10:36:40 +0800

    generator 自动生成mapper和model

---
+++ mall-generator/src/main/java/com/haochang/mall/mapper/UmsMemberLevelMapper.java
@@ -0,0 +1,30 @@
+package com.haochang.mall.mapper;
+
+import com.haochang.mall.model.UmsMemberLevel;
+import com.haochang.mall.model.UmsMemberLevelExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UmsMemberLevelMapper {
+    long countByExample(UmsMemberLevelExample example);
+
+    int deleteByExample(UmsMemberLevelExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UmsMemberLevel record);
+
+    int insertSelective(UmsMemberLevel record);
+
+    List<UmsMemberLevel> selectByExample(UmsMemberLevelExample example);
+
+    UmsMemberLevel selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") UmsMemberLevel record, @Param("example") UmsMemberLevelExample example);
+
+    int updateByExample(@Param("record") UmsMemberLevel record, @Param("example") UmsMemberLevelExample example);
+
+    int updateByPrimaryKeySelective(UmsMemberLevel record);
+
+    int updateByPrimaryKey(UmsMemberLevel record);
+}
